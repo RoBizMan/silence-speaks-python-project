bf5c5e21c5d97cc65f61ad7b5a9c24befba93f92
Robert Tiffert <152310537+RoBizMan@users.noreply.github.com>
2025-01-18 18:35:45 +0100

    Add Python unit testing for REST API and set debug to false

---
+++ rest_microservice/tests/test_routes.py
@@ -0,0 +1,28 @@
+import sys
+import os
+import unittest
+
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+)
+
+from app import create_app 
+
+class FlaskAppTests(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client()
+        self.app.testing = True
+
+    def test_home_page(self):
+        response = self.client.get('/echo')
+        self.assertEqual(response.status_code, 200)
+        json_data = response.get_json()
+        self.assertIn("message", json_data)
+        self.assertIn("timestamp", json_data)
+        self.assertEqual(
+            json_data["message"], "Send a POST request with a message."
+        )
+
+if __name__ == '__main__':
+    unittest.main()
